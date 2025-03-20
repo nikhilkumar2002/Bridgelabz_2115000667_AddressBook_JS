346fe29390bd644935e2a5225b38152071fb63d2
nikhilkumar2002 <nikunikhil0409@gmail.com>
2025-03-20 22:30:24 +0530

    [Nikhil] Added Regix

--- AddressBook.js
+++ AddressBook.js
@@ -1,5 +1,14 @@
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.validateName(firstName, "First Name");
+        this.validateName(lastName, "Last Name");
+        this.validateAddress(address, "Address");
+        this.validateAddress(city, "City");
+        this.validateAddress(state, "State");
+        this.validateZip(zip);
+        this.validatePhoneNumber(phoneNumber);
+        this.validateEmail(email);
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -10,32 +19,52 @@ class Contact {
         this.email = email;
     }
 
+    validateName(name, fieldName) {
+        const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        if (!nameRegex.test(name)) {
+            throw new Error(`${fieldName} must start with a capital letter and have at least 3 characters.`);
+        }
+    }
+
+    validateAddress(value, fieldName) {
+        if (value.length < 4) {
+            throw new Error(`${fieldName} must have at least 4 characters.`);
+        }
+    }
+
+    validateZip(zip) {
+        const zipRegex = /^[1-9][0-9]{5}$/;
+        if (!zipRegex.test(zip)) {
+            throw new Error(`Zip code must be a 6-digit number and not start with 0.`);
+        }
+    }
+
+    validatePhoneNumber(phone) {
+        const phoneRegex = /^[6-9][0-9]{9}$/;
+        if (!phoneRegex.test(phone)) {
+            throw new Error(`Phone number must be a valid 10-digit number starting with 6-9.`);
+        }
+    }
+
+    validateEmail(email) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            throw new Error(`Invalid email format.`);
+        }
+    }
+
     toString() {
         return `Name: ${this.firstName} ${this.lastName}\nAddress: ${this.address}, ${this.city}, ${this.state} ${this.zip}\nPhone: ${this.phoneNumber}\nEmail: ${this.email}`;
     }
 }
 
-class AddressBook {
-    constructor() {
-        this.contacts = [];
-    }
-
-    addContact(contact) {
-        this.contacts.push(contact);
-    }
-
-    displayContacts() {
-        this.contacts.forEach(contact => console.log(contact.toString() + "\n"));
-    }
-}
-
 // Example Usage
-let addressBook = new AddressBook();
+try {
+    let contact1 = new Contact("Nikhil", "Kumar", "BHEL Jhansi", "Jhansi", "UP", "284120", "9876543210", "nikhil.kumar@gmail.com");
+    console.log(contact1.toString());
 
-let contact1 = new Contact("Nikhil", "Kumar", "BHEL Jhansi", "Jhansi", "UP", "284120", "1234567890", "nikhil.kumar@gmail.com");
-let contact2 = new Contact("Amit", "Sharma", "Civil Lines", "Delhi", "DL", "110001", "9876543210", "amit.sharma@gmail.com");
-
-addressBook.addContact(contact1);
-addressBook.addContact(contact2);
-
-addressBook.displayContacts();
+    let contact2 = new Contact("Amit", "Sharma", "Delhi NCR", "Delhi", "DL", "110001", "9123456789", "amit.sharma@gmail.com");
+    console.log(contact2.toString());
+} catch (error) {
+    console.error(error.message);
+}
